7a4e5c9f8a457f7ed84a562392eb7142f78b42ca
Shikadeep <shikhadeep74@gmail.com>
2025-04-29 18:54:35 +0530

    s

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -30,7 +30,7 @@ const Blog = () => {
     <div className="bg-[#FDECF2] py-12  md:px-8 lg:px-20 px-[70px]">
       <div className="max-w-screen-xl mx-auto text-center font-poppins">
         <h1 className="text-[#1200FF] text-3xl md:text-4xl font-medium mb-8">Blog</h1>
-        <div className='flex gap-2 flex-col md:w-full'>
+        <div className='flex gap-7 flex-col md:w-full'>
         <div className="flex flex-wrap justify-center  gap-7">
           {blogList.map((blog, index) => (
             <div key={index} className=" w-full sm:w-[47%] lg:w-[22%]">
@@ -38,7 +38,7 @@ const Blog = () => {
             </div>
           ))}
         </div>
-        <div className="flex flex-wrap justify-center gap-6 ">
+        <div className="flex flex-wrap justify-center gap-7 ">
           {blogList.map((blog, index) => (
             <div key={index} className="w-full sm:w-[47%]  lg:w-[22%]">
               <BlogCard {...blog} />
--- src/components/Blog/BlogCard.jsx
+++ src/components/Blog/BlogCard.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const BlogCard = ({ Blogimg, title, description }) => {
   return (
-    <div className="flex flex-col gap-2 font-poppins  rounded-xl transition hover:shadow-lg">
+    <div className="flex flex-col gap-2 bg-white font-poppins  rounded-xl transition hover:shadow-lg">
       <img 
         src={Blogimg} 
         alt={title}
